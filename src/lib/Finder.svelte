f393fac1d42bf02a0c7688d4f67022babc978e88
Salvador Ortiz <sog@msg.mx>
2023-10-21 19:59:02 -0600

    Add Finder, proper svelte component

---
+++ src/lib/Finder.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import "../app.scss";
+	import { onMount } from 'svelte';
+	import { Wrapper } from './wrapper';
+
+	let container: HTMLDivElement;
+	onMount(() => {
+		if (container) {
+			const wrapper = new Wrapper({
+				container,
+				callback: (event) => {
+					console.log('Event:', event);
+				}
+			});
+		}
+
+	});
+</script>
+
+<div bind:this={container} />
