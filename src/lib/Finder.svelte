baa17eb7218c8df65b65386e01c44e7b7a3d7bce
Salvador Ortiz <sog@msg.mx>
2023-10-27 13:05:57 -0600

    Finder accepts config and callbacks

--- src/lib/Finder.svelte
+++ src/lib/Finder.svelte
@@ -1,16 +1,28 @@
-<script lang="ts">
-	import "../app.scss";
+<script lang="ts" context="module">
+	import type { IconFinderWrapperParams } from "./wrapper/params";
+	import type { FinderOptions } from "./config/components";
+	type FinderCallback = IconFinderWrapperParams['callback'];
+	export type { FinderCallback, FinderOptions};
 	import { onMount } from 'svelte';
 	import { Wrapper } from './wrapper';
+	export type FinderCtrl = Wrapper;
+</script>
+<script lang="ts">
+	import "./style/style.scss";
 
+	export let finder: FinderCtrl | null = null;
+	// With defaults if caller don't set any
+	export let finderOpts: FinderOptions = {};
+	export let callback: FinderCallback = (event) => {
+		console.log('Event:', event);
+	};
 	let container: HTMLDivElement;
 	onMount(() => {
 		if (container) {
-			const wrapper = new Wrapper({
+			finder = new Wrapper({
 				container,
-				callback: (event) => {
-					console.log('Event:', event);
-				}
+				callback,
+				finder: finderOpts
 			});
 		}
 
