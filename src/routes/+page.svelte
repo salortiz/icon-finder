d5ee0e1a5806f82a892e3cd9fb7cbbb33eb7a311
Salvador Ortiz <sog@msg.mx>
2023-10-31 11:02:37 -0600

    Use example in +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,22 @@
 <script lang="ts">
 	import Finder from '$lib/Finder.svelte'
+	import type { FinderCtrl, FinderOptions, FinderCallback } from '$lib/Finder.svelte';
+	const finderOpts: FinderOptions = {
+		showCustomisatons: false,
+		footer: 'Compact'
+	};
+	let finder: FinderCtrl;
+	const callback: FinderCallback =
+		(event) => {
+			if(event.type == 'load') {
+				console.log('Finder loaded!')
+			}
+			if(event.type == 'button') {
+				if(event.button == 'cancel')
+					finder.selectIcons([]);
+			}
+			console.log('Event:', event);
+		};
 </script>
 
-<Finder />
+<Finder bind:finder={finder} {finderOpts} {callback}/>
