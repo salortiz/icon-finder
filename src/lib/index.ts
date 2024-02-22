c08d20d37fb8daaca47163dc9e827ffee8b5403a
Salvador Ortiz <sog@msg.mx>
2024-02-22 11:16:04 -0600

    Export IFCore namespace to our consumers

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,4 +1,5 @@
 import Finder from "./Finder.svelte";
 export type { FinderCallback, FinderCtrl, FinderOptions } from "./Finder.svelte";
+import * as IFCore from "@iconify/search-core";
 import { Wrapper } from "./wrapper.js";
-export{ Finder, Wrapper };
+export { Finder, Wrapper, IFCore };
